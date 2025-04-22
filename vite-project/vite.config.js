69743120f66f4c4a30f145cda6576362d323f0a8
vijay239999 <vijaykuruva96@gmail.com>
2025-04-22 22:19:41 +0530

    vrock

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    server: {
+      host: '0.0.0.0', // Bind to external interface
+      port: 5173,      // Ensure the correct port is used
+    },  
 })
